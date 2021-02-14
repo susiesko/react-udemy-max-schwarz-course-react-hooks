12631d4f5e8130915010b79e609f4d464904d083
susiesko <susie.sko@gmail.com>
2021-02-14 14:55:16 -0800

    after lecture 432

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,8 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const [inputState, setInputState] = useState({title: '', amount: ''});
+  const [amount, setAmount] = useState('');
+  const [title, setTitle] = useState('');
 
   const submitHandler = event => {
     event.preventDefault();
@@ -20,14 +21,8 @@ const IngredientForm = React.memo(props => {
             <input 
               type="text"
               id="title"
-              value={inputState.title}
-              onChange={event => {
-                const newTitle = event.target.value;
-                setInputState(prevState => ({ 
-                  title: newTitle, 
-                  amount: prevState.amount 
-                }))
-              }}
+              value={title}
+              onChange={event => setTitle(event.target.value)}
             />
           </div>
           <div className="form-control">
@@ -35,15 +30,8 @@ const IngredientForm = React.memo(props => {
             <input 
               type="number" 
               id="amount" 
-              value={inputState.amount} 
-              onChange={event => {
-                const newAmount = event.target.value;
-
-                return setInputState(prevState => ({ 
-                  title: prevState.title, 
-                  amount: newAmount 
-                })
-              )}}
+              value={amount} 
+              onChange={event => setAmount(event.target.value)}
             />
           </div>
           <div className="ingredient-form__actions">
