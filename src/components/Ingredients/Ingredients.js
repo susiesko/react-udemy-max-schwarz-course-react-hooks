4f0866d5a5449c934a55bacaa2ded9e9cf66c2fa
Susie <susie.sko@gmail.com>
2021-02-14 15:21:53 -0800

    after module assignment

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -11,13 +11,17 @@ const Ingredients = () => {
     setUserIngredients(prevIngredients => [...prevIngredients, { id: Math.random().toString(), ...ingredient }]);
   };
 
+  const removeIngredientHandler = id => {
+    setUserIngredients(prevIngredients => prevIngredients.filter(ing => ing.id !== id));
+  }
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
