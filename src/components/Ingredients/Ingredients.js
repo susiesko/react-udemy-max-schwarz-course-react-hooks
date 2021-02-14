c4371cebd6e380769132fd1017f7792175ee658d
Susie <susie.sko@gmail.com>
2021-02-14 15:42:45 -0800

    after lecture 435

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,14 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
 
 const Ingredients = () => {
+  const baseFetchURL = 'https://react-hooks-module-2ba54-default-rtdb.firebaseio.com'
   const [ userIngredients, setUserIngredients ] = useState([]);
 
+  useEffect(() => {
+    fetch(`${baseFetchURL}/ingredients.json`)
+    .then(response => response.json())
+    .then(data => {
+      const loadedIngredients = [];
+      for (const key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount
+        })
+      }
+      //setUserIngredients(loadedIngredients);
+    });
+  }, []);
+
   const addIngredientHandler = ingredient => {
-    fetch('https://react-hooks-module-2ba54-default-rtdb.firebaseio.com/ingredients.json', {
+    fetch(`${baseFetchURL}/ingredients.json`, {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: {
