39f947c50379b8dd1a97f3a73ac64c41c264e1e4
Susie <susie.sko@gmail.com>
2021-02-14 15:32:53 -0800

    after lecture 434

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,7 +8,17 @@ const Ingredients = () => {
   const [ userIngredients, setUserIngredients ] = useState([]);
 
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [...prevIngredients, { id: Math.random().toString(), ...ingredient }]);
+    fetch('https://react-hooks-module-2ba54-default-rtdb.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      return response.json();
+    }).then(data => {
+      setUserIngredients(prevIngredients => [...prevIngredients, { id: data.name, ...ingredient }]);
+    });
   };
 
   const removeIngredientHandler = id => {
