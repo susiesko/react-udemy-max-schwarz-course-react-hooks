ec187845dd4078165f513b8778cbfbf8c9c896df
Susie <susie.sko@gmail.com>
2021-02-14 20:10:44 -0800

    after lecture 439

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -7,23 +7,28 @@ const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   const baseFetchURL = 'https://react-hooks-module-2ba54-default-rtdb.firebaseio.com';
   const [filter, setFilter] = useState('');
+  const inputRef = useRef();
   
   useEffect(() => {
-    const query = filter.length === 0 ? '' : `?orderBy="title"&equalTo="${filter}"`;
+    setTimeout(() => {
+      if (filter === inputRef.current.value){
+        const query = filter.length === 0 ? '' : `?orderBy="title"&equalTo="${filter}"`;
     
-    fetch(`${baseFetchURL}/ingredients.json${query}`)
-    .then(response => response.json())
-    .then(data => {
-      const loadedIngredients = [];
-      for (const key in data) {
-        loadedIngredients.push({
-          id: key,
-          title: data[key].title,
-          amount: data[key].amount
-        })
+        fetch(`${baseFetchURL}/ingredients.json${query}`)
+        .then(response => response.json())
+        .then(data => {
+          const loadedIngredients = [];
+          for (const key in data) {
+            loadedIngredients.push({
+              id: key,
+              title: data[key].title,
+              amount: data[key].amount
+            })
+          }
+          onLoadIngredients(loadedIngredients);
+        });
       }
-      onLoadIngredients(loadedIngredients);
-    });
+    }, 500);
   }, [filter, onLoadIngredients]);
 
   return (
@@ -31,7 +36,9 @@ const Search = React.memo(props => {
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text"
+          <input 
+            ref={inputRef}
+            type="text"
             value={filter}
             onChange={ev => setFilter(ev.target.value)} />
         </div>
